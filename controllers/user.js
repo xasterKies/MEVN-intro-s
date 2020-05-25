012c7a59c639ae2bac9c0e7bf79832b9cd17d3e0
SamuelEdward <samueledward40@gmail.com>
2020-05-25 13:02:22 -0700

    Express completed

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 module.exports.controller = (app) => {
     //get users page
     app.get('/users', (req, res) => {
-        res.render('index', { title: 'Users' });
+        res.render('users', { title: 'Users', description: 'This is the description of all the users' });
     })
 }
