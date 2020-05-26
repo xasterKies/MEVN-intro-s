ae1f1a14cd0d5c00298cc1e109d6fb4c31ff4740
SamuelEdward <samueledward40@gmail.com>
2020-05-26 13:23:16 -0700

    Creating a resource

--- models/User.js
+++ models/User.js
@@ -12,5 +12,21 @@ const UserSchema = new Schema({
     email: String
 })
 
+const user_resource = new User({
+    name: 'John Doe',
+    email: 'john@doe.com'
+})
+
+user_resource.save((error) => {
+    if(error)
+        console.log(error);
+
+    res.send({
+        success: true,
+        code: 200,
+        msg: "User added!"
+    })
+})
+
 const User = mongoose.model('User', UserSchema)
 module.exports = Users
