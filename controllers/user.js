8af7d21bb2e905850f4a512fe13c79612b68c859
SamuelEdward <samueledward40@gmail.com>
2020-05-27 05:05:43 -0700

    deleting descrpition

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 module.exports.controller = (app) => {
     //get users page
     app.get('/users', (req, res) => {
-        res.render('users', { title: 'Users', description: 'This is the description of all the users' });
+        res.render('index', { title: 'Users'});
     })
 }
