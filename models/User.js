579473880a05c80a74116d43fd2b964ee191f3ba
SamuelEdward <samueledward40@gmail.com>
2020-05-27 05:03:04 -0700

    formating for REST Api

--- models/User.js
+++ models/User.js
@@ -1,32 +1,10 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
-
 const Schema = new Schema({
     name: String,
     email: String
 })
 
-const UserSchema = new Schema({
-    name: String, 
-    email: String
-})
-
-const user_resource = new User({
-    name: 'John Doe',
-    email: 'john@doe.com'
-})
-
-user_resource.save((error) => {
-    if(error)
-        console.log(error);
-
-    res.send({
-        success: true,
-        code: 200,
-        msg: "User added!"
-    })
-})
 
 const User = mongoose.model('User', UserSchema)
 module.exports = Users
