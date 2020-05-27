d344bd35e544b550cea3fccf894a39e5b10bcc3d
SamuelEdward <samueledward40@gmail.com>
2020-05-27 09:22:57 -0700

    Modifing app.js properly for postman

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
 
 var app = express();
 //connect to mongodb
-mongoose.connect('mongodb://localhost:27017/MEVN-intro-s', function() {
-  console.log('Connection has been made');
-})
-.catch(err => {
-  console.error('App starting error:', err.stack);
-  process.exit(1);
+mongoose.connect('mongodb://localhost:27017/MEVN-intro-s', function(){
+  useMongoClient: true
+  console.log('Connection has been made')
+});
+var db = mongoose.connections;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback){
+  console.log("Connection Succeeded")
 });
 
 //Require file system module
@@ -30,7 +33,10 @@ fs.readdirSync('controllers').forEach(function (file) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//uncomment after placing our favicon in /public
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const Schema = new Schema({
+const UserSchema = new Schema({
     name: String,
     email: String
 })
 
 
 const User = mongoose.model('User', UserSchema)
-module.exports = Users
+module.exports = User
