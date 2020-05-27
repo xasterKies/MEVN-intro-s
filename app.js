6fa1bebe49bae7632a2246e2e3c58e3038263d9b
SamuelEdward <samueledward40@gmail.com>
2020-05-27 09:26:22 -0700

    fixing as in teacher

--- app.js
+++ app.js
@@ -60,9 +60,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-//app.listen(3000, function() { console.log('listening on 3000')})
-
+module.exports = app;
 
 app.listen(3000, () => { console.log('listening on 3000')})
-
-module.exports = app;
